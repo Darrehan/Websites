1fe40e985c6e4bf968094751f567b10ae857e2bb
Your Name <darrehanrasool@gmail.com>
2025-03-01 19:04:26 +0530

    Dar Rehan Rasool
    sycomusiclover

--- SimpleWebsite/app.js
+++ SimpleWebsite/app.js
@@ -1,20 +1,26 @@
-//Node js by default uses commen.js 
-const express=require("express");
-const morgan=require("morgan");
-const home=require("./routes/Home.js");
-const dashboard=require("./routes/Dashboard.js");
-const path =require("path");
-const http=require("http");
+const express = require("express");
+const morgan = require("morgan");
+const home = require("./routes/Home.js");
+const dashboard = require("./routes/Dashboard.js");
+const path = require("path");
+const http = require("http");
 const bodyParser = require("body-parser");
-const app=express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"public")));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan("combined"));
 app.use(home);
 app.use(dashboard);
-app.use((req,res,next)=>{
-res.status(404).sendFile(path.join(__dirname,"views","Error.html"));
-})
-const port=3000;
-const server =http.createServer(app);
-server.listen(port);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "Error.html"));
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server running at: http://localhost:${port}`);
+});
